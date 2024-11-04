d0a70a16d520a89e5e26e7286553b65086777d68
aquashdw <aquashdw@gmail.com>
2024-11-04 14:32:44 +0900

    remove cam manually or automatically

--- app/routes/camera.$name.tsx
+++ app/routes/camera.$name.tsx
@@ -1,6 +1,22 @@
-import { LoaderFunctionArgs, redirect } from "@remix-run/node";
-import { json, useLoaderData } from "@remix-run/react";
+import { ActionFunctionArgs, LoaderFunctionArgs, redirect } from "@remix-run/node";
+import { Form, json, useBeforeUnload, useLoaderData, useSubmit } from "@remix-run/react";
 import { getSession } from "~/lib/session";
+
+export const action = async({
+  params, request
+}: ActionFunctionArgs) => {
+  const session = await getSession(request);
+  if (!session.get("signedIn")) return redirect("/signin");
+  const name = params.name;
+  if (!name) throw new Response("Bad Request", { status: 400 });
+  await fetch(`http://localhost:8080/cameras/${name}`, {
+    method: "delete",
+    headers: {
+      "Authorization": `Bearer ${session.get("jwt")}`,
+    },
+  });
+  return redirect("/");
+};
 
 export const loader = async ({
   params, request
@@ -25,6 +41,13 @@ export const loader = async ({
 export default function Camera() {
   const { name, token } = useLoaderData<typeof loader>();
   console.log(token);
+
+  const submit = useSubmit();
+  useBeforeUnload(() => {
+    console.log("useBeforeUnload");
+    submit(new FormData(),{method: "post"})
+  });
+
   // useEffect(() => {
   //   const socket = new WebSocket("ws://localhost:8080/ws/connect?name=camera");
   //   socket.addEventListener("open", (event) => {
@@ -43,7 +66,12 @@ export default function Camera() {
   // }, []);
   return (
     <main>
-      <h1>Camera {name}</h1>
+      <div className="main-content-centered">
+        <h1 className="mb-4">Camera {name}</h1>
+        <Form method="post">
+          <input type="submit" value="Remove Camera"/>
+        </Form>
+      </div>
     </main>
   )
 }
